2293a7c9b71e0d9d572e3f8a77823e8343ffb8a4
Dominick Serwe <dom.serwe@gmail.com>
2025-02-12 22:47:43 -0500

    ecs first attempt

--- src/World.h
+++ src/World.h
@@ -3,8 +3,25 @@
 
 #include "LinearMath.h"
 
+#include <any>
+#include <map>
 #include <vector>
+#include <typeindex>
+#include <tuple>
 #include <unordered_map>
+#include <unordered_set>
+
+#ifdef WIN32
+#define DEBUG_BREAK() __debugbreak()
+#elif __linux__
+#define DEBUG_BREAK() __builtin_debugtrap()
+#elif __APPLE__
+#define DEBUG_BREAK() __builtin_trap()
+#else
+#define DEBUG_BREAK()
+#endif
+
+#define ASSERT(condition) do { if (!(condition)) { DEBUG_BREAK(); } } while(0)
 
 struct SDL_Renderer;
 
@@ -20,33 +37,133 @@ struct Entity {
     std::vector<Vec2> path;
 };
 
-class EntityStorage {
-public:
-    Entity* CreateEntity();
-    void DestroyEntity(EntityID id);
-
-    Entity* GetEntity(EntityID id);
-
-    std::vector<Entity>::iterator begin() {
-        return m_entities.begin();
-    }
-
-    std::vector<Entity>::iterator end() {
-        return m_entities.end();
-    }
-
-private:
-    std::vector<Entity> m_entities;
-    std::unordered_map<EntityID, int> m_entityMap;
+struct PhysicsComponent {
+    Vec2 position;
+    Vec2 velocity;
+    Vec2 size;
 };
 
-struct World {
-    EntityStorage entityStorage;
+struct ColorComponent {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+};
 
-    EntityID EntityAtPosition(const Vec2& position);
+struct PathComponent {
+    std::vector<Vec2> points;
+};
 
-    void DoPhysics(float deltaTime);
-    void Draw(SDL_Renderer* renderer);
+struct EntityInfo {
+    EntityID id;
+    std::unordered_map<std::type_index, int> components;
+};
+
+template<typename T>
+struct ComponentPair {
+    EntityID id;
+    T component;
+};
+
+class EntityStorage {
+public:
+    template<typename T>
+    void Register() {
+        std::type_index typeID = typeid(T);
+
+        if (m_componentData.contains(typeID)) {
+            return;
+        }
+
+        m_componentData.emplace(typeID, std::vector<ComponentPair<T>>());
+    }
+
+    template<typename T>
+    T& CreateComponent(EntityID entity) {
+        ASSERT(entity != 0);
+        std::type_index typeID = typeid(T);
+
+        ASSERT(m_entityInfoMap.contains(entity));
+        EntityInfo& info = m_entityInfoMap[entity];
+
+        if (info.components.contains(typeID)) {
+            int index = info.components[typeID];
+
+            ASSERT(m_componentData.contains(typeID));
+            std::vector<ComponentPair<T>>& data = std::any_cast<std::vector<ComponentPair<T>>&>(m_componentData[typeID]);
+
+            return data[index].component;
+        }
+
+        ASSERT(m_componentData.contains(typeID));
+        std::vector<ComponentPair<T>>& data = std::any_cast<std::vector<ComponentPair<T>>&>(m_componentData[typeID]);
+
+        int index = data.size();
+
+        data.push_back({ entity, T() });
+        info.components.emplace(typeID, index);
+
+        return data.back().component;
+    }
+
+    template<typename T>
+    T& GetComponent(EntityID entity) {
+        ASSERT(entity != 0);
+        std::type_index typeID = typeid(T);
+
+        ASSERT(m_entityInfoMap.contains(entity));
+        EntityInfo info = m_entityInfoMap[entity];
+
+        ASSERT(info.components.contains(typeID));
+        int index = info.components[typeID];
+
+        std::vector<ComponentPair<T>>& data = GetComponents<T>();
+
+        return data[index].component;
+    }
+
+    template<typename T>
+    std::vector<ComponentPair<T>>& GetComponents() {
+        std::type_index typeID = typeid(T);
+
+        ASSERT(m_componentData.contains(typeID));
+        return std::any_cast<std::vector<ComponentPair<T>>&>(m_componentData[typeID]);
+    }
+
+    std::vector<EntityID> GetQuery(const std::vector<std::type_index>& query) {
+        std::vector<EntityID> result;
+
+        for (const auto& [ id, info ] : m_entityInfoMap) {
+            int validCount = 0;
+
+            for (const auto& [ typeID, index ] : info.components) {
+                bool valid = false;
+                
+                for (std::type_index queryTypeID : query) {
+                    if (queryTypeID == typeID) {
+                        valid = true;
+                        break;
+                    }
+                }
+
+                if (valid) {
+                    validCount++;
+                }
+            }
+
+            if (validCount == query.size()) {
+                result.push_back(id);
+            }
+        }
+
+        return result;
+    }
+
+    EntityID CreateEntity();
+
+private:
+    std::unordered_map<EntityID, EntityInfo> m_entityInfoMap;
+    std::unordered_map<std::type_index, std::any> m_componentData;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,67 @@
 #include "core/Application.h"
 #include "World.h"
 
+static void MoveEntityAlongPath(PhysicsComponent& physics, PathComponent& path) {
+    if (path.points.size() < 2) {
+        return;
+    }
+
+    Vec2 targetPoint = path.points[0];
+    Vec2 moveVector = targetPoint - physics.position;
+
+    if (moveVector.Length() < 1) {
+        path.points.erase(path.points.begin());
+
+        targetPoint = path.points[0];
+        moveVector = targetPoint - physics.position;
+    }
+
+    physics.velocity = moveVector.Normalized() * 75;
+}
+
 class App : public Application {
 public:
     void OnInit() override {
+        m_entityStorage.Register<PhysicsComponent>();
+        m_entityStorage.Register<ColorComponent>();
+        m_entityStorage.Register<PathComponent>();
+
         {
-            Entity* entity = m_world.entityStorage.CreateEntity();
+            EntityID entity = m_entityStorage.CreateEntity();
 
-            entity->position.x = 1280 / 2.0f;
-            entity->position.y = 720 / 2.0f;
+            PhysicsComponent& physics = m_entityStorage.CreateComponent<PhysicsComponent>(entity);
+            ColorComponent& color = m_entityStorage.CreateComponent<ColorComponent>(entity);
+            m_entityStorage.CreateComponent<PathComponent>(entity);
 
-            entity->size.x = 32;
-            entity->size.y = 32;
+            physics.position.x = 1280 / 2.0f;
+            physics.position.y = 720 / 2.0f;
+
+            physics.size.x = 32;
+            physics.size.y = 32;
+
+            color.r = 0;
+            color.g = 0;
+            color.b = 255;
+            color.a = 255;
         }
 
         {
-            Entity* entity = m_world.entityStorage.CreateEntity();
+            EntityID entity = m_entityStorage.CreateEntity();
 
-            entity->position.x = 300;
-            entity->position.y = 200;
+            PhysicsComponent& physics = m_entityStorage.CreateComponent<PhysicsComponent>(entity);
+            ColorComponent& color = m_entityStorage.CreateComponent<ColorComponent>(entity);
+            m_entityStorage.CreateComponent<PathComponent>(entity);
 
-            entity->size.x = 32;
-            entity->size.y = 32;
+            physics.position.x = 300;
+            physics.position.y = 200;
+
+            physics.size.x = 32;
+            physics.size.y = 32;
+
+            color.r = 0;
+            color.g = 0;
+            color.b = 255;
+            color.a = 255;
         }
     }
 
@@ -32,12 +72,16 @@ public:
         Vec2 mousePos = { (float)mouseX, (float)mouseY };
 
         if ((mouseState & SDL_BUTTON(SDL_BUTTON_LEFT)) && !m_canDrawPath) {
-            m_selectedEntity = m_world.EntityAtPosition(mousePos);
-            Entity* entity = m_world.entityStorage.GetEntity(m_selectedEntity);
+            //m_selectedEntity = m_world.EntityAtPosition(mousePos);
+            //Entity* entity = m_world.entityStorage.GetEntity(m_selectedEntity);
 
-            if (entity) {
+            m_selectedEntity = EntityAtPosition(mousePos);
+
+            if (m_selectedEntity != 0) {
+                auto& path = m_entityStorage.GetComponent<PathComponent>(m_selectedEntity);
+                
                 m_canDrawPath = true;
-                entity->path.clear();
+                path.points.clear();
             }
         }
         else if (!(mouseState & SDL_BUTTON(SDL_BUTTON_LEFT)) && m_canDrawPath) {
@@ -47,16 +91,93 @@ public:
 
         if (m_canDrawPath) {
             if (mousePos != m_lastMousePos) {
-                Entity* entity = m_world.entityStorage.GetEntity(m_selectedEntity);
-                entity->path.push_back(mousePos);
+                auto& path = m_entityStorage.GetComponent<PathComponent>(m_selectedEntity);
+                path.points.push_back(mousePos);
             }
 
             m_lastMousePos = mousePos;
         }
 
-        m_world.DoPhysics(timeStep.DeltaTime());
-        m_world.Draw(m_renderer);
+        DoPhysics(timeStep);
+        DoRender(m_renderer);
     }
+private:
+    EntityID EntityAtPosition(const Vec2& position) {
+        const auto& physicsData = m_entityStorage.GetComponents<PhysicsComponent>();
+
+        for (const ComponentPair<PhysicsComponent>& data : physicsData) {
+            SDL_FRect rect = {
+                data.component.position.x - (data.component.size.x / 2.0f),
+                data.component.position.y - (data.component.size.y / 2.0f),
+                data.component.size.x,
+                data.component.size.y,
+            };
+
+            if ((position.x <= rect.x + rect.w) && (position.x >= rect.x) && (position.y <= rect.y + rect.h) && (position.y >= rect.y)) {
+                return data.id;
+            }
+        }
+        
+        return 0;
+    }
+
+    void DoPhysics(TimeStep timeStep) {
+        std::vector<EntityID> query = m_entityStorage.GetQuery({ typeid(PhysicsComponent), typeid(PathComponent) });
+    
+        for (EntityID entity : query) {
+            PhysicsComponent& physics = m_entityStorage.GetComponent<PhysicsComponent>(entity);
+            PathComponent& path = m_entityStorage.GetComponent<PathComponent>(entity);
+    
+            MoveEntityAlongPath(physics, path);
+    
+            physics.position += physics.velocity * timeStep.DeltaTime();
+    
+            float x0 = physics.position.x - (physics.size.x / 2.0f);
+            float x1 = x0 + physics.size.x;
+    
+            float y0 = physics.position.y - (physics.size.y / 2.0f);
+            float y1 = y0 + physics.size.y;
+    
+            if (x0 <= 0 || x1 >= 1280) {
+                physics.velocity.x = -physics.velocity.x;
+            }
+    
+            if (y0 <= 0 || y1 >= 720) {
+                physics.velocity.y = -physics.velocity.y;
+            }
+        }
+    }
+
+    void DoRender(SDL_Renderer* renderer) {
+        std::vector<EntityID> query = m_entityStorage.GetQuery({ typeid(PhysicsComponent), typeid(PathComponent), typeid(ColorComponent)});
+    
+        for (EntityID entity : query) {
+            auto& physics = m_entityStorage.GetComponent<PhysicsComponent>(entity);
+            auto& path = m_entityStorage.GetComponent<PathComponent>(entity);
+            auto& color = m_entityStorage.GetComponent<ColorComponent>(entity);
+    
+            SDL_FRect rect = {
+                physics.position.x - (physics.size.x / 2.0f),
+                physics.position.y - (physics.size.y / 2.0f),
+                physics.size.x,
+                physics.size.y,
+            };
+    
+            if (path.points.size() > 0) {
+                for (int i = 0; i < path.points.size() - 1; i++) {
+                    Vec2 start = path.points[i];
+                    Vec2 end = path.points[i + 1];
+    
+                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                    SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
+                }
+            }
+    
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+            SDL_RenderFillRectF(renderer, &rect);
+        }
+    }
+
 private:
     Uint64 m_lastTime = 0;
 
@@ -64,7 +185,7 @@ private:
     EntityID m_selectedEntity = 0;
     Vec2 m_lastMousePos;
 
-    World m_world;
+    EntityStorage m_entityStorage;
 };
 
 int main(int argc, char** argv) {
